10efb573b33b6875b767bf1b45ae2c6850368185
ProgettoMedico <50099542+ProgettoMedico@users.noreply.github.com>
2019-05-24 11:48:52 +0200

    Inserimento Nuovo utente-Verifica esistenza

---
+++ Inserimento Nuovo utente-Verifica esistenza/NuovoUtente/NuovoUtente/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace NuovoUtente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCrea = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCognome = new System.Windows.Forms.TextBox();
+            this.txtCodFiscale = new System.Windows.Forms.TextBox();
+            this.txtDataNascita = new System.Windows.Forms.TextBox();
+            this.txtIndirizzo = new System.Windows.Forms.TextBox();
+            this.txtSesso = new System.Windows.Forms.TextBox();
+            this.txtNumeroTel = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnTrovaUtente = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            this.label1.Visible = false;
+            // 
+            // btnCrea
+            // 
+            this.btnCrea.Location = new System.Drawing.Point(231, 240);
+            this.btnCrea.Name = "btnCrea";
+            this.btnCrea.Size = new System.Drawing.Size(75, 23);
+            this.btnCrea.TabIndex = 2;
+            this.btnCrea.Text = "Crea Utente";
+            this.btnCrea.UseVisualStyleBackColor = true;
+            this.btnCrea.Visible = false;
+            this.btnCrea.Click += new System.EventHandler(this.btnCrea_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(176, 95);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 3;
+            this.txtNome.Visible = false;
+            // 
+            // txtCognome
+            // 
+            this.txtCognome.Location = new System.Drawing.Point(47, 95);
+            this.txtCognome.Name = "txtCognome";
+            this.txtCognome.Size = new System.Drawing.Size(100, 20);
+            this.txtCognome.TabIndex = 4;
+            this.txtCognome.Visible = false;
+            // 
+            // txtCodFiscale
+            // 
+            this.txtCodFiscale.Location = new System.Drawing.Point(51, 39);
+            this.txtCodFiscale.Name = "txtCodFiscale";
+            this.txtCodFiscale.Size = new System.Drawing.Size(160, 20);
+            this.txtCodFiscale.TabIndex = 5;
+            // 
+            // txtDataNascita
+            // 
+            this.txtDataNascita.Location = new System.Drawing.Point(176, 149);
+            this.txtDataNascita.Name = "txtDataNascita";
+            this.txtDataNascita.Size = new System.Drawing.Size(100, 20);
+            this.txtDataNascita.TabIndex = 6;
+            this.txtDataNascita.Visible = false;
+            // 
+            // txtIndirizzo
+            // 
+            this.txtIndirizzo.Location = new System.Drawing.Point(47, 199);
+            this.txtIndirizzo.Name = "txtIndirizzo";
+            this.txtIndirizzo.Size = new System.Drawing.Size(100, 20);
+            this.txtIndirizzo.TabIndex = 7;
+            this.txtIndirizzo.Visible = false;
+            // 
+            // txtSesso
+            // 
+            this.txtSesso.Location = new System.Drawing.Point(176, 199);
+            this.txtSesso.Name = "txtSesso";
+            this.txtSesso.Size = new System.Drawing.Size(100, 20);
+            this.txtSesso.TabIndex = 8;
+            this.txtSesso.Visible = false;
+            // 
+            // txtNumeroTel
+            // 
+            this.txtNumeroTel.Location = new System.Drawing.Point(47, 149);
+            this.txtNumeroTel.Name = "txtNumeroTel";
+            this.txtNumeroTel.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeroTel.TabIndex = 9;
+            this.txtNumeroTel.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Cognome";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Codice Fiscale";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(173, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Data Nascita";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Indirizzo";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(173, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Sesso";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(44, 133);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Numero Telefono";
+            this.label7.Visible = false;
+            // 
+            // btnTrovaUtente
+            // 
+            this.btnTrovaUtente.Location = new System.Drawing.Point(222, 36);
+            this.btnTrovaUtente.Name = "btnTrovaUtente";
+            this.btnTrovaUtente.Size = new System.Drawing.Size(84, 23);
+            this.btnTrovaUtente.TabIndex = 16;
+            this.btnTrovaUtente.Text = "Trova Utente";
+            this.btnTrovaUtente.UseVisualStyleBackColor = true;
+            this.btnTrovaUtente.Click += new System.EventHandler(this.btnTrovaUtente_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 281);
+            this.Controls.Add(this.btnTrovaUtente);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumeroTel);
+            this.Controls.Add(this.txtSesso);
+            this.Controls.Add(this.txtIndirizzo);
+            this.Controls.Add(this.txtDataNascita);
+            this.Controls.Add(this.txtCodFiscale);
+            this.Controls.Add(this.txtCognome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnCrea);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCrea;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCognome;
+        private System.Windows.Forms.TextBox txtCodFiscale;
+        private System.Windows.Forms.TextBox txtDataNascita;
+        private System.Windows.Forms.TextBox txtIndirizzo;
+        private System.Windows.Forms.TextBox txtSesso;
+        private System.Windows.Forms.TextBox txtNumeroTel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnTrovaUtente;
+    }
+}
+
---
+++ Inserimento Nuovo utente-Verifica esistenza/NuovoUtente/NuovoUtente/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; //Aggiungiamo alla libreria
+
+namespace NuovoUtente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string connectionstring = @"Data Source=(local);Initial Catalog=Progetto Medico;Integrated Security=True"; //Stringa di connessione
+
+        private void btnCrea_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                SqlConnection cnn = new SqlConnection(connectionstring);
+
+                {
+                    cnn.Open();
+
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Utenti (CodFiscale,Nome,Cognome,DataNascita,Indirizzo,Sesso,NumeroTelefono) values ( @CodFiscale, @Nome, @Cognome,@DataNascita,@Indirizzo,@Sesso,@NumeroTelefono)", cnn); //Inseriamo il nuovo utente
+
+
+                    cmd.Parameters.AddWithValue("@CodFiscale", txtCodFiscale.Text); //Stampiamo dalla text-box
+                    cmd.Parameters.AddWithValue("@Nome", txtNome.Text);
+                    cmd.Parameters.AddWithValue("@Cognome", txtCognome.Text);
+                    cmd.Parameters.AddWithValue("@DataNascita", txtDataNascita.Text);
+                    cmd.Parameters.AddWithValue("@Indirizzo", txtIndirizzo.Text);
+                    cmd.Parameters.AddWithValue("@Sesso", txtSesso.Text);
+                    cmd.Parameters.AddWithValue("@NumeroTelefono", txtNumeroTel.Text);
+
+
+                    cmd.ExecuteNonQuery();
+                    cnn.Close(); //Chiudiamo la connessione
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnTrovaUtente_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                SqlConnection cnn = new SqlConnection(connectionstring);
+
+                {
+
+                    cnn.Open(); //Apriamo la connessione
+                    SqlCommand cmd = new SqlCommand("SELECT COUNT (CodFiscale) FROM Utenti WHERE CodFiscale = @CF", cnn); //Verifichiamo se il codice fiscale è presente nel database e ritorna 0 o 1 se esiste
+                    cmd.Parameters.AddWithValue("CF", txtCodFiscale.Text); //Stampiamo dalla text-box
+                    Int32 count = Convert.ToInt32(cmd.ExecuteScalar()); 
+
+                    if (count == 1)
+                    {
+                        MessageBox.Show("L'utente esiste già!");
+                    }
+
+                    else //Creiamo il nuovo utente
+                    {
+                        btnCrea.Visible = true;
+                        label1.Visible = true;
+                        label2.Visible = true;
+                        label3.Visible = true;
+                        label4.Visible = true;
+                        label5.Visible = true;
+                        label6.Visible = true;
+                        label7.Visible = true;
+                        txtNome.Visible = true;
+                        txtCognome.Visible = true;
+                        txtDataNascita.Visible = true;
+                        txtIndirizzo.Visible = true;
+                        txtSesso.Visible = true;
+                        txtNumeroTel.Visible = true;
+                    }
+                    cnn.Close(); //Chiudiamo la connessione
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
